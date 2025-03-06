43c45560236648e939a99b3a9edbc797ae0c49aa
azeemhdr <comazeem@gmail.com>
2025-03-06 16:10:29 +0530

    css changes done

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { removeTodo } from "../app/features/todo/todoSlice";
 export default function Todo() {
-  const todos = useSelector(state => state.todos);
+  const todos = useSelector((state) => state.todos);
   const dispatch = useDispatch();
 
-	return (
+  return (
     <>
-      <div>TODO's</div>
-	  {todos.map((item) => (
-		<li key={item.id}>
-			{item.text}
-			<button onClick={()=>dispatch(removeTodo(item.id))}>X</button>
-		</li>
-))}
+      <div className="bg-[#f9a702] p-3">
+        <h2 className="text-red-600 font-semibold p-3 ">TODO's</h2>
+        {todos.map((item) => (
+          <li key={item.id} className="w-full flex justify-between items-center gap-5 ps-5  bg-white rounded-md">
+            <div className="w-full text-start text-green-900 font-semibold capitalize">{item.text}</div>
+            <button onClick={() => dispatch(removeTodo(item.id))}>X</button>
+          </li>
+        ))}
+      </div>
     </>
   );
 }
